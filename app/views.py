a13c1a2c944d1c4b376b529773b88fe1e7fee2d0
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-29 15:25:39 +0500

    api changes

--- app/views.py
+++ app/views.py
@@ -10,6 +10,8 @@ from .serilializers import (
     CertificatesSerializer,
     CompanySerializer,
 )
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -44,3 +46,42 @@ class ProductTranslationViewSet(viewsets.ModelViewSet):
 class ProductImageViewSet(viewsets.ModelViewSet):
     queryset = ProductImage.objects.all()
     serializer_class = ProductImageSerializer
+
+
+class ProductByCategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        category_id = self.request.query_params.get('category')  # from ?category=1
+        if category_id is not None:
+            return Product.objects.filter(category_id=category_id)
+        return Product.objects.none()
+
+class ProductView(APIView):
+    def get(self, request, pk):
+        product = Product.objects.filter(pk=pk).first()
+        serializer = ProductSerializer(product)
+
+        return Response(serializer.data)
+    
+
+class ProductTranslationViewDetail(APIView):
+    def get(self, request):
+        product_id = self.request.query_params.get('product')
+        language_id = self.request.query_params.get('language')
+        product =  ProductTranslation.objects.filter(product_id=product_id, language_id=language_id)
+        print(product)
+        if product.exists():
+            print("here1")
+            serializer = ProductTranslationSerializer(product,many=True)
+            return Response(serializer.data)
+        else:
+            print("here2")
+            default_lang = Language.objects.get(is_default=True)
+            print(default_lang)
+            default_product = ProductTranslation.objects.filter(product_id=product_id, language_id=default_lang.id).first()
+            if default_product:
+                serializer = ProductTranslationSerializer(default_product)
+                return Response(serializer.data)
+        return Response(status=404)
+
