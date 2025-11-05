c70bb9de90f889a772d7400ff461b8cbbbaa45ae
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-11-05 15:39:57 +0500

    jazzmin

--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,7 +127,7 @@ BACKEND_URL = 'http://localhost:8000'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
